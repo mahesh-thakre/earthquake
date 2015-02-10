60360ab2317c48dd8f5c950e57b9deb5128a666d
macuser <macuser@macusers-Air.fios-router.home>
2015-02-09 18:26:07 -0600

    Initial Commit

---
+++ EarthquakeMonitor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EarthquakeMonitor
+//
+//  Created by macuser on 2/9/15.
+//  Copyright (c) 2015 macuser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
